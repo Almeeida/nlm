7e0a450dab0b59e180628b8bd1c221c9b7e3adfc
Almeeida <rodrigofigueire19@gmail.com>
2020-06-05 20:18:58 +0100

    backend fix

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,17 +1,34 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
-import { ColumnNameQueryBuilder } from 'knex';
 
 class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+    const parsedItems = String(items)
+      .split(',')
+      .map((i) => Number(i.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  }
+
   async show(req: Request, res: Response) {
     const { id } = req.params;
 
     const point = await knex('points').where('id', id).first();
     if (!point) return res.status(400).json({ message: 'Point not found.' });
 
-    const items = await knex('points')
+    const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
-      .where('point_items.point_id', id);
+      .where('point_items.point_id', id)
+      .select('items.title');
 
     return res.json({ point, items });
   }
@@ -30,7 +47,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       latitude,
@@ -44,9 +61,11 @@ class PointsController {
     const pointItems = items.map((item_id: Number) => ({
       item_id,
       point_id
-    }))
+    }));
 
     await trx('point_items').insert(pointItems);
+
+    await trx.commit();
 
     return res.json({
       id: point_id,
